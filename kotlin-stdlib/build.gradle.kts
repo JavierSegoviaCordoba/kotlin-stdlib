50168a27e84042fa6cb4e20558433b60c7918474
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-11-04 20:55:42 +0100

    Add `javiersc-publish` config and all Kotlin Multiplatform targets

--- kotlin-stdlib/build.gradle.kts
+++ kotlin-stdlib/build.gradle.kts
@@ -3,12 +3,37 @@ import org.jetbrains.dokka.gradle.DokkaTask
 plugins {
     `kotlin-multiplatform`
     `javiersc-kotlin-library`
+    `javiersc-publish`
 }
 
 kotlin {
     explicitApi()
 
+    iosArm64()
+    iosSimulatorArm64()
+    iosX64()
+
     jvm()
+
+    js(BOTH) {
+        browser()
+        nodejs()
+    }
+
+    linuxX64()
+
+    macosArm64()
+    macosX64()
+
+    mingwX64()
+
+    tvosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+
+    watchosArm64()
+    watchosSimulatorArm64()
+    watchosX64()
 }
 
 tasks.withType<DokkaTask> {
